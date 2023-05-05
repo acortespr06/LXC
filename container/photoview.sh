eb79b1d31533faaa89af2ac2aa776ba6dee09041
Andres Cortes <67211732+acortespr06@users.noreply.github.com>
2023-05-05 10:42:07 -0500

    Update photoview.sh

--- container/photoview.sh
+++ container/photoview.sh
@@ -2,7 +2,7 @@
 
 # Update and upgrade computer
 sudo apt update
-sudo apt upgrade
+yes | sudo apt upgrade
 
 # Install necessary tools
 sudo apt install git curl wget software-properties-common libdlib-dev libblas-dev libatlas-base-dev liblapack-dev libjpeg-turbo8-dev build-essential libdlib19 libdlib-dev libjpeg-dev libheif-dev pkg-config gpg
@@ -13,7 +13,7 @@ sudo add-apt-repository ppa:strukturag/libde265
 
 # Install Node 16 and NPM
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt install nodejs
+yes | sudo apt install nodejs
 
 # Download and install Go
 wget https://golang.org/dl/go1.16.linux-amd64.tar.gz
@@ -27,13 +27,13 @@ cd /opt
 git clone https://github.com/photoview/photoview.git
 cd photoview/ui
 npm install
-npx update-browserslist-db@latest
+yes | npx update-browserslist-db@latest
 curl -o /opt/photoview/ui/vite.config.js https://raw.githubusercontent.com/acortespr06/LXC/main/misc/config/vite.config.js
 npm run build
 
 # Build the API back-end and copy needed files
 cd ../api
-go build -v -o photoview .
+yes | go build -v -o photoview .
 cd ..
 mkdir app
 cp -r ui/build/ app/ui/
